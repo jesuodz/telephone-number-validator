64a22f594e276d8d4efabd177ef33d150435461a
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-07-25 08:40:34 -0400

    Add best possible regex pattern

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const validator = {};
 // (\d{3}|\-\))?(\d{4})IMPORTANT
-var pattern = /(\(|^1|\d{3})[\-]?(\s\(?|\s?|\(?)\d{3}(\)\s|\s|\))*/;
+var pattern = /(^[+1]{1}|\()?[\s|\-]?(\s\(|\()?\d{3}(\)\s|\s|\)|\-)?\d{3}(\s|\-)?\d{4}/;
 
 validator.telephoneChecker = function(str) {
     return pattern.test(str);
